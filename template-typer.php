65cb3febe9f45f1115a371edc3a705844dde5408
Kerry O'Connor <kerryoco@gmail.com>
2022-06-24 02:34:51 -0400

    save

---
+++ template-typer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Typer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package pbc
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		woohoo typer
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
