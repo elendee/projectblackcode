b61948c9511c387e23b914c115b5f0f759f0f01f
kerry o <kerryoco@gmail.com>
2024-03-04 13:07:07 -0500

    cache

--- js/init_typer.js
+++ js/init_typer.js
@@ -1,7 +1,7 @@
-import { Modal } from './Modal.js'
-import hal from './hal.js'
-import spinner from './hal.js'
-import typer from './hackertyper/typer.js'
+import { Modal } from './Modal.js?v=115'
+import hal from './hal.js?v=115'
+// import spinner from './hal.js'
+import typer from './hackertyper/typer.js?v=115'
 
 
 console.log('typer' ) // Modal, hal, spinner
