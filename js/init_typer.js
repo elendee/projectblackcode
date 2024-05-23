bebca758d01613733238f8379b82b9ccb95ef470
kerry o <kerryoco@gmail.com>
2024-05-23 19:07:36 -0400

    cache

--- js/init_typer.js
+++ js/init_typer.js
@@ -16,3 +16,25 @@ for( const link of document.querySelectorAll('link')){
 if( !has_wc ){
 	console.error('woocommerce must be enabled for proper site function')
 }
+
+
+
+// product page format amazon link
+const add_cart = document.querySelector('form.cart button[type=submit]')
+if( add_cart ){
+	const amazon_link = document.querySelector('.pbc-amazon-link')
+	if( amazon_link ){
+		const btn = document.createElement('a')
+		btn.classList.add('button', 'amazon-link')
+		btn.style['text-decoration'] = 'none'
+		btn.innerText = 'Buy this on Amazon'
+		btn.href = amazon_link.innerText
+		btn.target = '_blank'
+		add_cart.parentElement.insertBefore( btn, add_cart )
+		add_cart.remove()
+		const quant = document.querySelector('.quantity input')
+		if( quant ) quant.remove()
+		amazon_link.remove()
+	}
+}
+
